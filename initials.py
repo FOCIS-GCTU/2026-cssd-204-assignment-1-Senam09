ae3a1d54fc091d38f01efed0330faaeaa66fe265
Senam09 <2425402306@live.gctu.edu.gh>
2026-06-01 15:56:00 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: #Printing large letters of my initials using mini letters of them
+# Assignment Number: 2
+#
+# Name: Menye Senam
+# STUDENT ID:  2425402306
+# Email: menyesenam15@gmail.com
+# Grader: Augustus Buckman
+# On my honor, Menye Senam, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+#Printing large letters of my initials using mini letters of them
 
 
+
+print()
+print("...MSF")
+print()
+print("....M..........M........SSSSSSSSSSSS........FFFFFFFFFFFF....")
+print("....MM........MM........S...................F...............")
+print("....M.M......M.M........S...................F...............")
+print("....M..M....M..M........S...................F...............")
+print("....M...M..M...M........SSSSSSSSSSSS........FFFFFFFFFFF.....")
+print("....M....MM....M...................S........F...............")
+print("....M..........M****...............S****....F...........****")
+print("....M..........M****...............S****....F...........****")
+print("....M..........M****....SSSSSSSSSSSS****....F...........****")
+print("....M..........M****................****....F...........****")
+print()
 
 main()
